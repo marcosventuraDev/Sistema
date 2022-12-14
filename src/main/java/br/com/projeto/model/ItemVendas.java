d0f8ee2d1f87e8c42d81fc449eeffd416c40a1e7
marcos ventura <marcosventura.dev@gmail.com>
2022-12-14 16:33:19 -0300

    implementando classe Vendas e itemVendas

---
+++ src/main/java/br/com/projeto/model/ItemVendas.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.projeto.model;
+
+/**
+ *
+ * @author joaom
+ */
+public class ItemVendas {
+    
+    //atibutos 
+    
+    private int id;
+    private Vendas vendas;
+    private Produtos produtos;
+    private int qtd;
+    private double subtotal;
+    
+    
+    
+    //getters e setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Vendas getVendas() {
+        return vendas;
+    }
+
+    public void setVendas(Vendas vendas) {
+        this.vendas = vendas;
+    }
+
+    public Produtos getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(Produtos produtos) {
+        this.produtos = produtos;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+    
+}
