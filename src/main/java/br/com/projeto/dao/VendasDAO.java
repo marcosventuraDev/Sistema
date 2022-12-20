25de43c652685ea020628253d3dcd3c820c87a61
marcos ventura <marcosventura.dev@gmail.com>
2022-12-20 15:45:08 -0300

    implementando metodo de busca de vendas por data

--- src/main/java/br/com/projeto/dao/VendasDAO.java
+++ src/main/java/br/com/projeto/dao/VendasDAO.java
@@ -5,11 +5,14 @@
 package br.com.projeto.dao;
 
 import br.com.projeto.jdbc.ConnectionFactory;
+import br.com.projeto.model.Clientes;
 import br.com.projeto.model.Vendas;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -74,6 +77,45 @@ public class VendasDAO {
         }
         
 }
+    
+    //Metodo que filtra vendas por data
+    
+    public List<Vendas> listarVendasPorPeriodo(String data_inicio, String data_fim){
+        try {
+            
+            List<Vendas> lista = new ArrayList<>();
+            
+            String sql = "SELECT v.id, v.data_venda, c.nome, v.total_venda, "
+                    + " v.observacoes FROM tb_vendas as v INNER JOIN tb_clientes as c"
+                    + " on (v.cliente_id = c.id) WHERE  v.datavenda BETWEEN ?  "
+                    + "ADN ?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, data_inicio);
+            stmt.setString(2, data_fim);
+            
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                Vendas obj = new Vendas();
+                Clientes c = new Clientes();
+                
+                obj.setId(rs.getInt("v.id"));
+                obj.setData_venda(rs.getString("v.data_venda"));
+                c.setNome(rs.getString("c.nome"));
+                obj.setTotal_venda(rs.getDouble("v.total_venda"));
+                obj.setObservacoes(rs.getString("v.observacoes"));
+                
+                
+                obj.setCliente(c);
+                
+                lista.add(obj);
+            }
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Erro na busca por data"+ e);
+        }
+        
+    }
     
     
 }
